de63affc3d76c3c54b63d54ecedd7e886594d611
Daniel Alejandro <119262827+Daniel62882@users.noreply.github.com>
2023-12-06 14:02:00 -0500

    Add files via upload

--- proyecto_violencia/Deployment-DEV/service/flask_service.py
+++ proyecto_violencia/Deployment-DEV/service/flask_service.py
@@ -1,45 +1,9 @@
-# Importar las bibliotecas necesarias
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-from tensorflow.keras.models import load_model
-from werkzeug.utils import secure_filename
-import cv2
 import numpy as np
-from model_loader import loadModelH5
+from model_loader import loaded_model_vgg16, loaded_model_cnn  # Asegúrate de importar los modelos correctos
+import cv2
 
-# Args
-import argparse
-
-# Configurar los argumentos del programa
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--port", required=True, help="Número de puerto del servicio.")
-args = vars(ap.parse_args())
-
-# Puerto del servicio
-port = args['port']
-print("Puerto reconocido:", port)
-
-# Parámetros
-UPLOAD_FOLDER = 'uploads/videos'
-ALLOWED_EXTENSIONS = set(['mp4'])
-
-# Inicializar la aplicación Flask
 app = Flask(__name__)
-CORS(app)
-
-# Cargar el modelo VGG16
-MODEL_VGG16_FILE = "violencia_model_full_tf2.h5"  
-MODEL_VGG16_PATH = "/home/dasniel298/models/model/tf3x/keras/full/"  
-loaded_model_vgg16 = load_model(MODEL_VGG16_PATH + MODEL_VGG16_FILE)
-
-# Cargar el modelo CNN
-MODEL_CNN_FILE = "violencia_model_full_tf2.h5"  
-MODEL_CNN_PATH = "/home/dasniel298/models/model/tf2x/keras/full/"  
-loaded_model_cnn = load_model(MODEL_CNN_PATH + MODEL_CNN_FILE)
-
-# Funciones
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def read_video(path):
     frames = []
@@ -54,85 +18,35 @@ def read_video(path):
     return frames
 
 def conv_feature_image(frames):
-    max_frames = 190
-    frames = frames[:max_frames]
-
-    while len(frames) < max_frames:
-        frames.append(np.zeros((224, 224, 3)))
-
-    conv_features = [loaded_model_vgg16.predict(np.expand_dims(frame, axis=0))[0] for frame in frames]
-
+    conv_features = loaded_model_vgg16.predict(np.array(frames))
     return np.array(conv_features)
 
 def resize_zeros(img_features, max_frames):
-    # Verificar si img_features tiene 3 dimensiones
-    if len(img_features.shape) == 3:
-        rows, col, _ = img_features.shape
-    else:
-        # Si img_features no tiene 3 dimensiones, establecer valores predeterminados
-        rows, col = img_features.shape[0], img_features.shape[1]
-
-    # Asegúrate de que el tamaño de la última dimensión sea 3
-    zero_matrix = np.zeros((max_frames - rows, col, 3)) if len(img_features.shape) == 3 else np.zeros((max_frames - rows, col))
-
-    # Asegúrate de que img_features tenga 3 dimensiones
-    img_features = img_features if len(img_features.shape) == 3 else np.expand_dims(img_features, axis=-1)
-
+    rows, cols = img_features.shape[:2]  # Obtener solo las dos primeras dimensiones
+    zero_matrix = np.zeros((max_frames - rows, cols))
     return np.concatenate((img_features, zero_matrix), axis=0)
 
-
-
-# Ruta para clasificar videos
 @app.route('/model/predict/', methods=['POST'])
 def predict_video():
-    data = {"success": False}
-    if request.method == "POST":
-        if 'file' not in request.files:
-            print('No se recibió el archivo')
-            return jsonify(data)
+    try:
+        # Obtener el archivo de video desde la solicitud
+        video_file = request.files['file']
+        video_path = "uploads/videos/" + video_file.filename
+        video_file.save(video_path)
 
-        file = request.files['file']
-        if file.filename == '':
-            print('No se seleccionó ningún archivo')
-            return jsonify(data)
+        # Procesar el video
+        frames = read_video(video_path)
+        img_features = conv_feature_image(frames)
+        img_features_resized = resize_zeros(img_features, 190)
 
-        if file and allowed_file(file.filename):
-            print("\nNombre de archivo recibido:", file.filename)
-            filename = secure_filename(file.filename)
-            tmpfile = ''.join([UPLOAD_FOLDER, '/', filename])
-            file.save(tmpfile)
-            print("\nNombre de archivo almacenado:", tmpfile)
+        # Hacer la predicción
+        prediction = loaded_model_cnn.predict(np.array([img_features_resized]))
+        result = {"prediction": float(prediction[0])}
 
-            # Procesar video con modelo VGG16
-            frames = read_video(tmpfile)
-            img_features = conv_feature_image(frames)
-            img_features = resize_zeros(img_features, 190)
+        return jsonify(result)
 
-            # Predecir con el modelo VGG16 cargado
-            predictions_vgg16 = loaded_model_vgg16.predict(np.array([img_features]))[0]
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-            # Procesar video con modelo CNN
-            img_features_cnn = resize_zeros(img_features, 190)
-
-            # Predecir con el modelo CNN cargado
-            predictions_cnn = loaded_model_cnn.predict(np.array([img_features_cnn]))[0]
-
-            # Tomar la decisión final basada en ambas predicciones
-            overall_prediction = "violencia" if predictions_cnn >= 0.5 else "no-violencia"
-            class_prob = float(predictions_cnn)
-
-            print("Etiqueta de predicción:", overall_prediction)
-            print("Probabilidad de predicción: {:.2%}".format(class_prob))
-
-            # Resultados en formato Json
-            data["predictions"] = [{"label": overall_prediction, "score": class_prob}]
-
-            # Éxito
-            data["success"] = True
-
-    return jsonify(data)
-
-# Ejecutar la aplicación Flask
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
